c15f5ae9eb9f5e3ac1dce6571dd507814ccdfe01
abighuan <abighuan@126.com>
2016-08-24 18:21:01 +0800

    新增切换城市和手动更新天气的功能

--- src/com/coolweatherbh/app/activity/ChooseAreaActivity.java
+++ src/com/coolweatherbh/app/activity/ChooseAreaActivity.java
@@ -14,7 +14,10 @@ import com.coolweatherbh.app.util.Utility;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -79,6 +82,14 @@ public class ChooseAreaActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		if (prefs.getBoolean("city_selected", false)) {
+			Intent intent = new Intent(this, WeatherActivity.class);
+			startActivity(intent);
+			finish();
+			return;
+		}
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.choose_area);
 		listView = (ListView) findViewById(R.id.list_view);
@@ -93,18 +104,22 @@ public class ChooseAreaActivity extends Activity {
 			public void onItemClick(AdapterView<?> arg0, View view, int index,
 					long arg3) {
 				if (currentLevel == LEVEL_PROVINCE) {
-					Log.d("ChooseAreaActivity", "222222222222222");
 					selectedProvince = provinceList.get(index);
 					queryCities();
 				} else if (currentLevel == LEVEL_CITY) {
-					Log.d("ChooseAreaActivity", "33333333333333333");
 					selectedCity = cityList.get(index);
 					queryCounties();
+				} else if (currentLevel == LEVEL_COUNTY) {
+					String countyCode = countyList.get(index).getCountyCode();
+					Intent intent = new Intent(ChooseAreaActivity.this,
+							WeatherActivity.class);
+					intent.putExtra("county_code", countyCode);
+					startActivity(intent);
+					finish();
 				}
 			}
 
 		});
-		Log.d("ChooseAreaActivity", "111111111111");
 		queryProvinces();// 加载省级数据
 	}
 
@@ -228,7 +243,6 @@ public class ChooseAreaActivity extends Activity {
 			public void onError(Exception e) {
 				// 通过runOnUiThread()方法回到主线程处理逻辑
 				runOnUiThread(new Runnable() {
-
 					@Override
 					public void run() {
 						closeProgressDialog();
--- src/com/coolweatherbh/app/util/Utility.java
+++ src/com/coolweatherbh/app/util/Utility.java
@@ -1,5 +1,15 @@
 package com.coolweatherbh.app.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 
 import com.coolweatherbh.app.db.CoolWeatherDB;
@@ -75,6 +85,51 @@ public class Utility {
 			}
 		}
 		return false;
+	}
+
+	/**
+	 * 解析服务器返回的JSON数据,并将解析出的数据存储到本地
+	 */
+	/**
+	 * {"weatherinfo":{"city":"昆 山","cityid":"101190404","temp1":"4℃","temp2":
+	 * "16℃","weather":"晴转多 云","img1":"n0.gif","img2":"d1.gif","ptime":"18:00"}}
+	 * 城市名,温度范围,天气信息描述,发布时间这几项需要显示在界面上,
+	 */
+	public static void handleWeatherResponse(Context context, String response) {
+		try {
+			JSONObject jsonObject = new JSONObject(response);
+			JSONObject weatherInfo = jsonObject.getJSONObject("weatherinfo");
+			String cityName = weatherInfo.getString("city");
+			String weatherCode = weatherInfo.getString("cityid");
+			String temp1 = weatherInfo.getString("temp1");
+			String temp2 = weatherInfo.getString("temp2");
+			String weatherDesp = weatherInfo.getString("weather");
+			String publishTime = weatherInfo.getString("ptime");
+			saveWeatherInfo(context, cityName, weatherCode, temp1, temp2,
+					weatherDesp, publishTime);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 将服务器返回的所有天气信息存储到SharedPreferences文件中.
+	 */
+	public static void saveWeatherInfo(Context context, String cityName,
+			String weatherCode, String temp1, String temp2, String weatherDesp,
+			String publishTime) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy年M月d日", Locale.CHINA);
+		SharedPreferences.Editor editor = PreferenceManager
+				.getDefaultSharedPreferences(context).edit();
+		editor.putBoolean("city_selected", true);
+		editor.putString("city_name", cityName);
+		editor.putString("weather_code", weatherCode);
+		editor.putString("temp1", temp1);
+		editor.putString("temp2", temp2);
+		editor.putString("weather_desp", weatherDesp);
+		editor.putString("publish_time", publishTime);
+		editor.putString("current_date", sdf.format(new Date()));
+		editor.commit();
 	}
 
 }
